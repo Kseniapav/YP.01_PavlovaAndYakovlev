601491a5ecacb01f8a8d979f454ff587db2ab54e
Kseniapav <kseniapavlova5916@gmail.com>
2025-10-05 20:29:26 +0300

    делаю заново, добавила классы в библиотеку

---
+++ Testing/ClientsYchet/IClientRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientsYchet
+{
+    public interface IClientRepository
+    {
+        Client AddClient(Client client);
+        bool ExistsByEmailOrPhone(string email, string phone); //Проверяет, есть ли клиент с указанным email или телефоном
+        Client FindByEmailOrPhone(string email, string phone); //Возвращает клиента по контактным данным
+        bool DeleteByEmailOrPhone(string email, string phone); //Удаляет клиента по email или телефону
+    }
+}
