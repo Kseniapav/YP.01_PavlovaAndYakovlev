b2ebe054134a415cbedd97a6cb6a56103fa6b2a5
Kseniapav <kseniapavlova5916@gmail.com>
2025-10-28 20:35:06 +0300

    сделала подключение к бд

--- Testing/ClientsYchetsForms/MainForm.cs
+++ Testing/ClientsYchetsForms/MainForm.cs
@@ -7,17 +7,56 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace ClientsYchetsForms
 {
     public partial class MainForm: Form
     {
+        MySqlConnection connection = new MySqlConnection("Server = localhost; Database=clientsychet;Uid=root;Pwd=vertrigo;");
         public MainForm()
         {
             InitializeComponent();
+            LoadClients();
+        }
+        private void LoadClients()
+        {
+            try
+            {
+                connection.Open();
+
+                string query = "SELECT * FROM clients";
+
+                MySqlDataAdapter adapter = new MySqlDataAdapter(query, connection);
+                DataTable table = new DataTable();
+                adapter.Fill(table);
+
+                dataGridView1.DataSource = table;
+                dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+                dataGridView1.ReadOnly = true;
+                dataGridView1.AllowUserToAddRows = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка подключения или загрузки данных: " + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
         }
 
         private void buttonAdd_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
         }
