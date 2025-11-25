abf36ada40d7fe59e93bf5333e5a395189547add
Fa1tloy <baranovdaniil534@gmail.com>
2025-11-25 08:48:15 +0300

    добавила классы: Master,IMasterRepository,MasterRepository,MasterManager

---
+++ Testing/ClientsYchet/Master.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientsYchet
+{
+    public class Master
+    {
+        public int IdMaster { get; set; }
+        public string Name { get; set; }
+        public string Speciality { get; set; }
+        public string Number { get; set; }
+
+        public Master() { }
+
+        public Master(int idMaster, string name, string speciality, string number)
+        {
+            IdMaster = idMaster;
+            Name = name;
+            Speciality = speciality;
+            Number = number;
+        }
+    }
+}
---
+++ Testing/ClientsYchet/MasterManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientsYchet
+{
+    public class MasterManager: IDisposable
+    {
+        private IMasterRepository repository;
+        private List<Master> masters;
+
+        public MasterManager(IMasterRepository repository)
+        {
+            this.repository = repository;
+            masters = new List<Master>();
+        }
+
+        public List<Master> LoadMasters()
+        {
+            try
+            {
+                masters = repository.GetAllMasters();
+                return masters;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Ошибка загрузки данных: {ex.Message}");
+            }
+        }
+
+        public List<Master> GetMasters()
+        {
+            return masters;
+        }
+
+        public void RefreshMasters()
+        {
+            LoadMasters();
+        }
+
+        public void Dispose()
+        {
+            if (repository is IDisposable disposable)
+                disposable.Dispose();
+        }
+    }
+}
